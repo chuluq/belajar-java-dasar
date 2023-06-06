268106a46e114b70365bd5273252968c48ce5a1c
Moch Chaerul Chuluq <choirulchuluq10@gmail.com>
2023-06-06 14:50:10 +0700

    variable

---
+++ src/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Moch Chaerul Chuluq";
+
+        System.out.println(name);
+
+        name = "Mr Chuluq";
+        System.out.println(name);
+
+        var firstName = "Choirul";
+        var lastName = "Chuluq";
+
+        final String application = "Belajar Java";
+    }
+}
