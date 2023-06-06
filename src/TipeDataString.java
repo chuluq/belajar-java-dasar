aa78035b67f02ebc8de82f8c9dd1931b2b669331
Moch Chaerul Chuluq <choirulchuluq10@gmail.com>
2023-06-06 13:22:31 +0700

    tipe data char, string, boolean

---
+++ src/TipeDataString.java
@@ -0,0 +1,13 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Eko Kurniawan";
+        String lastname = "Khannedy";
+
+        String fullName = firstName + " " + lastname;
+
+        System.out.println(firstName);
+        System.out.println(lastname);
+
+        System.out.println(fullName);
+    }
+}
