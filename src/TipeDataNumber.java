849647f23017b5eb874bf7d635cfcbfb66c0218a
chuluq <chuluq@arvis.id>
2023-12-17 10:10:55 +0700

    feat: Number in Java

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,19 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 1000000;
+        long iniLong = 1000000;
+        long iniLong2 = 1000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.212345;
+
+        int decimalInt = 25;
+        int hexaInt = 0xFFFFFF;
+        int binaryInt = 0b10101010;
+
+        long balance = 1_000_000_000_000L;
+        int sum = 60_000_000;
+    }
+}
